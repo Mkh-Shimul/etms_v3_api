7ff86445da577c4cf1f4538ea7e8b16041e716a5
Mkh-Shimul <mkhshimul7@gmail.com>
2023-11-19 00:58:14 +0600

    user role model added

---
+++ Models/UserRole.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ETMS_API.Models
+{
+	public class UserRole
+	{
+		[Key]
+        public int Id { get; set; }
+        public string RoleName { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+        public int? CreateBy { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public int? UpdateAt { get; set; }
+        public DateTime? UpdatedBy { get; set; }
+    }
+}
