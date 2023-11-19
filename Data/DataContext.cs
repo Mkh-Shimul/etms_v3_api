45ecaea2452f254192868d6ff6a221fa64439c25
Mkh-Shimul <mkhshimul7@gmail.com>
2023-11-20 01:48:54 +0600

    user and user-role relationship configuration or fluent-api configuration added in the context class

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -11,5 +11,21 @@ namespace ETMS_API.Data
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Bus> Buses { get; set; }
         public DbSet<UserRole> UserRoles { get; set; }
-    }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<User>()
+				.HasKey(u => u.Id);
+
+			modelBuilder.Entity<UserRole>()
+				.HasKey(r => r.Id);
+
+			// Configure the relationship between User and UserRole
+			modelBuilder.Entity<User>()
+				.HasOne(u => u.UserRole)
+				.WithMany(r => r.Users)
+				.HasForeignKey(u => u.UserRoleId)
+				.OnDelete(DeleteBehavior.Restrict); // Specify the desired delete behavior if needed
+		}
+	}
 }
