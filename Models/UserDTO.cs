88eca7df7bacb8e6d11e1f58cce1ec2acec4dac1
Mkh-Shimul <mkhshimul7@gmail.com>
2023-11-20 01:47:09 +0600

    new input field added from the user-input for user model

--- Models/UserDTO.cs
+++ Models/UserDTO.cs
@@ -6,5 +6,6 @@
 		public string Email { get; set; } = string.Empty;
 		public string UserName { get; set; } = string.Empty;
 		public string Password { get; set; } = string.Empty;
-    }
+		public int UserRoleId { get; set; }
+	}
 }
