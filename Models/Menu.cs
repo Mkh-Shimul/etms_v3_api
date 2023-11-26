81ef75830e103cff068201f9d207a644215babd0
Mkh-Shimul <mkhshimul7@gmail.com>
2023-11-27 01:56:19 +0600

    menu table updated with userRoles relationship

---
+++ Models/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ETMS_API.Models
+{
+	public class Menu
+	{
+		[Key]
+		public int Id { get; set; }
+		public string MenuName { get; set; } = string.Empty;
+		public string MenuUrl { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+
+        public ICollection<UserRole>? UserRoles { get; set; }
+	}
+}
